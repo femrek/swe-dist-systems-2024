aa78db179b033b44f8dd4432b127c9e91995fb27
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-02-28 16:16:08 +0300

    feat: computer entity created.

--- desktop-app/src/main/java/com/swedist/desktopapp/model/Computer.java
+++ desktop-app/src/main/java/com/swedist/desktopapp/model/Computer.java
@@ -1,4 +1,45 @@
 package com.swedist.desktopapp.model;
 
 public class Computer {
+    private Long id;
+    private String brand;
+    private String model;
+
+    public Computer() {
+    }
+
+    public Computer(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Computer(Long id, String brand, String model) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
