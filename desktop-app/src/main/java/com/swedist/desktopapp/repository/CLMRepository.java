d18d10a17911f99a6ba548c73213b81f3a0a5b7c
demirhany <demirhanyilal0@gmail.com>
2024-02-28 16:21:02 +0300

    feat: Repository configuration is added and getStudent method is added

--- desktop-app/src/main/java/com/swedist/desktopapp/repository/CLMRepository.java
+++ desktop-app/src/main/java/com/swedist/desktopapp/repository/CLMRepository.java
@@ -1,13 +1,33 @@
 package com.swedist.desktopapp.repository;
 
+import com.swedist.desktopapp.model.Student;
+
 import java.sql.*;
 
 public class CLMRepository {
     // JDBC parameters
-    private final String URL = "jdbc:mysql://127.0.0.1:3306/cr";
+    private final String URL = "jdbc:postgresql://127.0.0.1:3306/cr";
     private final String USER = "root";
-    private final String PASSWORD = "mysql";
+    private final String PASSWORD = "1234";
     private Connection conn = null;
     private Statement stm = null;
     private ResultSet rs = null;
+
+    public Student getStudentById(int studentId) throws SQLException
+    {
+        conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        String query = "SELECT * FROM student WHERE id =" + studentId + ";";
+        stm = conn.createStatement();
+        rs = stm.executeQuery(query);
+
+        Student student = new Student();
+        while(rs.next())
+        {
+            student.setId(rs.getInt("id"));
+            student.setName(rs.getString("name"));
+            student.setDepartment(rs.getString("department"));
+        }
+        conn.close();
+        return student;
+    }
 }
