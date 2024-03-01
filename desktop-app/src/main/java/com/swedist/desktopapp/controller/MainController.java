91c40df47f7c42c44a2d62faa6e41bdebd4e9098
demirhany <demirhanyilal0@gmail.com>
2024-03-01 21:49:40 +0300

    Update: The onAction names are updated wrt. two view. Feat: Methods structuer are created for computer-view.

--- desktop-app/src/main/java/com/swedist/desktopapp/controller/MainController.java
+++ desktop-app/src/main/java/com/swedist/desktopapp/controller/MainController.java
@@ -5,65 +5,85 @@ import com.swedist.desktopapp.repository.CLMRepository;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
 import java.sql.SQLException;
 
 public class MainController {
     @FXML
     private Label welcomeText;
     @FXML
-    private TextField t1;
+    private TextField student_t1, student_t2, student_t3, computer_t1, computer_t2, computer_t3;
 
-    @FXML
-    private TextField t2;
-    @FXML
-    private TextField t3;
-
-
-//    @FXML
-
+    //FXML
     // Read Operation
     @FXML
-    protected void onFetchButtonClick() throws SQLException {
+    protected void onFetchButtonClickStudent() throws SQLException {
         CLMRepository repository = new CLMRepository();
         Student student;
-        student = repository.getStudentById(Integer.parseInt(t1.getText()));
-        t2.setText(student.getName());
-        t3.setText(student.getDepartment());
+        student = repository.getStudentById(Integer.parseInt(student_t1.getText()));
+        student_t2.setText(student.getName());
+        student_t3.setText(student.getDepartment());
     }
-
 
     // Create Operation
     @FXML
-    protected void onSaveButtonClick() throws Exception{
+    protected void onSaveButtonClickStudent() throws Exception{
         CLMRepository repository = new CLMRepository();
         Student student = new Student();
 //        student.setId(Integer.parseInt(t1.getText()));
-        student.setName(t2.getText());
-        student.setDepartment(t3.getText());
+        student.setName(student_t2.getText());
+        student.setDepartment(student_t3.getText());
         repository.addStudent(student);
     }
 
     //Update Operation
     @FXML
-    protected void onUpdateButtonClick() throws SQLException{
+    protected void onUpdateButtonClickStudent() throws SQLException{
         CLMRepository repository = new CLMRepository();
         Student student = new Student();
-        student.setId(Integer.parseInt(t1.getText()));
-        student.setName(t2.getText());
-        student.setDepartment(t3.getText());
+        student.setId(Integer.parseInt(student_t1.getText()));
+        student.setName(student_t2.getText());
+        student.setDepartment(student_t3.getText());
         repository.updateStudent(student);
     }
 
-
     //Delete Operation
     @FXML
-    protected void onDeleteButtonClick() throws SQLException{
+    protected void onDeleteButtonClickStudent() throws SQLException{
         CLMRepository repository = new CLMRepository();
         Student student = new Student();
-        student.setId(Integer.parseInt(t1.getText()));
-        student.setName(t2.getText());
-        student.setDepartment(t3.getText());
+        student.setId(Integer.parseInt(student_t1.getText()));
+        student.setName(student_t2.getText());
+        student.setDepartment(student_t3.getText());
         repository.deleteStudent(student);
+    }
+
+    @FXML
+    protected void onCloseButtonClickStudent()
+    {
+        System.exit(0);
+    }
+
+    @FXML
+    protected void onSaveButtonClickComputer()
+    {
+
+    }
+
+    @FXML
+    protected void onUpdateButtonClickComputer()
+    {
+
+    }
+
+    @FXML
+    protected void onDeleteButtonClickComputer()
+    {
+
+    }
+
+    @FXML
+    protected void onCloseButtonClickComputer()
+    {
+        System.exit(0);
     }
 }
