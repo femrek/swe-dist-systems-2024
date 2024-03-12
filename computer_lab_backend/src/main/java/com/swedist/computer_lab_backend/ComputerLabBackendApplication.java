c9971d4817f89f3d55e365fccc1e77f788f858a6
demirhany <demirhanyilal0@gmail.com>
2024-03-12 10:53:04 +0300

    computer_lab_backend has been created

---
+++ computer_lab_backend/src/main/java/com/swedist/computer_lab_backend/ComputerLabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swedist.computer_lab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerLabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerLabBackendApplication.class, args);
+    }
+    
+}
---
+++ computer_lab_backend/src/test/java/com/swedist/computer_lab_backend/ComputerLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swedist.computer_lab_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerLabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
