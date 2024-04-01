1068f1a7b3ec4157a63c7e16e28088757c9b6d63
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-04-01 14:57:33 +0300

    feat: bootstrap libraries are added to project instead of using cdn.
    style: linter errors.

--- computer_lab_backend/src/main/java/com/swedist/computer_lab_backend/ComputerLabBackendApplication.java
+++ computer_lab_backend/src/main/java/com/swedist/computer_lab_backend/ComputerLabBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComputerLabBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ComputerLabBackendApplication.class, args);
     }
-    
 }
