01324870105be10d6b05f84c56b968837c438540
Demirhan Yilal <demirhan.yilal@sakarya.edu.tr>
2024-05-03 00:21:58 +0300

    computer_lab_gradle project

---
+++ computer_lab_gradle/src/main/java/com/swedist/computer_lab_gradle/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.swedist.computer_lab_gradle.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
---
+++ computer_lab_gradle/src/main/java/com/swedist/computer_lab_gradle/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.swedist.computer_lab_gradle.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+}
