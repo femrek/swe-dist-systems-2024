a10a919e26bbde70c0bbcd83d3e926337551db13
Demirhan Yilal <demirhan.yilal@sakarya.edu.tr>
2024-05-08 13:11:07 +0300

    feat: jwt utilities and security implemented

--- computer_lab_gradle/src/main/java/com/swedist/computer_lab_gradle/controller/AuthController.java
+++ computer_lab_gradle/src/main/java/com/swedist/computer_lab_gradle/controller/AuthController.java
@@ -1,14 +1,32 @@
 package com.swedist.computer_lab_gradle.controller;
 
+import com.swedist.computer_lab_gradle.dto.AuthenticationRequest;
+import com.swedist.computer_lab_gradle.dto.RegisterRequest;
+import com.swedist.computer_lab_gradle.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @GetMapping("/login")
     public String loginPage() {
         return "auth/login";
     }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "auth/register";
+    }
+
 }
