add163b1d67ee3a5baf92ea7cb640cd30011ae29
Demirhan Yılal <92366725+demirhany@users.noreply.github.com>
2024-07-05 13:44:08 +0300

    Delete computer_lab_backend directory

--- computer_lab_backend/src/main/java/com/swedist/computer_lab_backend/repository/ComputerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swedist.computer_lab_backend.repository;
-
-import com.swedist.computer_lab_backend.model.Computer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComputerRepository extends JpaRepository<Computer, Long> {
-}
--- computer_lab_backend/src/main/java/com/swedist/computer_lab_backend/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swedist.computer_lab_backend.repository;
-
-import com.swedist.computer_lab_backend.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
