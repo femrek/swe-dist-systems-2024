927f106f0ac49f5944a1fb72a46e22afc3ca5eca
Demirhan Yılal <92366725+demirhany@users.noreply.github.com>
2024-07-05 13:42:46 +0300

    Delete computer_lab_gradle directory

--- computer_lab_gradle/src/main/java/com/swedist/computer_lab_gradle/repository/ComputerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swedist.computer_lab_gradle.repository;
-
-import com.swedist.computer_lab_gradle.entity.Computer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComputerRepository extends JpaRepository<Computer, Long> {
-}
